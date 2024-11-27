30df61cc659a2e37765e39ac930c3aeb74ab2881
MrIron2022 <2935156059@qq.com>
2024-11-27 16:30:40 +0800

    Add files via upload
    java 17; cloud 2023.0.0; boot 3.2.0; cloud alibaba 2022.0.0.0-RC2; maven 3.9.2; sql暂时没导入依赖

---
+++ springCloud2024/src/main/java/com/example/springcloud2024/SpringCloud2024Application.java
@@ -0,0 +1,13 @@
+package com.example.springcloud2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloud2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloud2024Application.class, args);
+    }
+
+}
---
+++ springCloud2024/src/test/java/com/example/springcloud2024/SpringCloud2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloud2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloud2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
